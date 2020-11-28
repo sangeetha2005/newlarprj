09a75b9eddcebee02b93b708bd6b3107b3a59d5e
sangeeth T <sangeethathangaraj@1cloubhub.com>
2020-11-28 22:46:20 +0530

    my first comit

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Sangeetha BlogPage</title>
+    </head>
+    <body>
+        <h1>My new blog page</h1>
+        <p>Welcome I'm sangeetha this is my new blog page!!!!</p>
+    </body>
+</html>
